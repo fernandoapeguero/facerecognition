cb142da5e2bd93701a1ed8824d36ce7f5df7fe2b
fernando peguero <fernandoapeguero@gmail.com>
2020-03-20 22:38:56 -0400

    created components for the app

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Navigation = () => {
+
+    return (
+        <nav style={{display: 'flex' , justifyContent: 'flex-end'}}>
+            <p className="underline link dim black pa3 f3 pointer">Sign Out</p>
+        </nav>
+    )
+
+}
+
+
+
+
+export default Navigation 
