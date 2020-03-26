586af483f315fbd114d717dea72c8f7912a77f50
fernando peguero <fernandoapeguero@gmail.com>
2020-03-26 00:25:13 -0400

    remove some un use information

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-import React , {Component} from 'react';
-=======
+
 import React, {Component} from 'react';
->>>>>>> 267a305a790073c0ec08be7004832c9866f631f0
 import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
@@ -10,21 +7,11 @@ import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './Components/FaceRecognition/FaceRecognition'
 import Rank from './Components/Rank/Rank';
 import Particles from 'react-particles-js';
-<<<<<<< HEAD
-import Clarifai from 'clarifai';
-
-const app = new Clarifai.App({
-  apiKey: 'ad4959b234064287b32da7697125d9b7'
- });
-=======
 import Clarifai from 'clarifai' 
 
 const app = new Clarifai.App({
   apiKey: 'ad4959b234064287b32da7697125d9b7'
 });
-
-
->>>>>>> 267a305a790073c0ec08be7004832c9866f631f0
 
 const particlesOptions = {
     particles: {
@@ -38,24 +25,13 @@ const particlesOptions = {
     }
 }
 
-class App extends Component {
-
-  constructor(){
-    super();
-    this.state = {
-       input: '',
-    }
-  }
-
-  onInputChange = (event) => {
-
 class App extends Component{
 
   constructor(){
     super();
-    this.state = {
-      input: '',
-      imageUrl: ''
+      this.state = {
+        input: '',
+        imageUrl: ''
     }
   }
 
@@ -68,14 +44,15 @@ class App extends Component{
   onButtonSubmit = () => {
 
     this.setState({imageUrl: this.state.input});
-
+    
     app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-    function(response) {
-      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-    },
-    function(err) {
-      console.log('Input is invalid ' + err)
-    }
+        function(response) {
+          console.log(response.outputs[0].data.regions[0]);
+          console.log("Hello")
+        },
+        function(err) {
+          console.log('Input is invalid ' + err)
+        }
   );
 
 }
